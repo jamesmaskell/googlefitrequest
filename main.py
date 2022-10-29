61f10c9ce771f3d47bca3f549430590d849a76ef
James Maskell <jameslmaskell@gmail.com>
2022-10-29 10:00:50 +0100

    tests

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from requests import post
 from datetime import datetime, timedelta
 from google.cloud import datastore
 
-def get_post_body():
-    epoch_today_start = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+def get_post_body(current_datetime):
+
+    epoch_today_start = current_datetime.replace(hour=0, minute=0, second=0, microsecond=0)
     epoch_today_end = epoch_today_start + timedelta(milliseconds=86399999)
 
     start = int(epoch_today_start.timestamp()) * 1000
@@ -24,8 +25,14 @@ def get_post_body():
 def get_header():
     db = datastore.Client()
 
+    if db == None:
+        return {}
+
     key = db.key("fit_tokens", "fit_tokens")
     token = db.get(key)
+
+    if token == None or 'access_token' not in token:
+        return {}
 
     return {
         'Authorization': f"Bearer {token['access_token']}"
@@ -33,12 +40,17 @@ def get_header():
 
 
 def execute(request):
-    response = post('https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate', json=get_post_body(),
+    response = post('https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate', json=get_post_body(datetime.now()),
                     headers=get_header())
-    step_count_delta_obj = response.json()
 
+    if response.status_code != 200:
+        print(f'bad response from fit api endpoint: {response.status_code}')
+        return { 'steps': 0 }
+
+    step_count_delta_obj = response.json()
     steps = 0
     for step_delta in step_count_delta_obj['bucket'][0]['dataset'][0]['point']:
         steps = steps + step_delta['value'][0]['intVal']
+        print(step_delta['value'][0]['intVal'], steps)
 
     return { 'steps': steps }
---
+++ tests/main_tests.py
@@ -0,0 +1,80 @@
+import datetime
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+import main
+
+class TestMain(TestCase):
+
+    def test_google_fit_request_body_created_with_correct_timestamps(self):
+        expected = {
+            'aggregateBy': [
+                {
+                    'dataTypeName': 'com.google.step_count.delta',
+                    'dataSourceId': 'derived:com.google.step_count.delta:com.google.android.gms:estimated_steps'
+                }
+            ],
+            'endTimeMillis': 1666997999999,
+            'startTimeMillis': 1666911600000
+        }
+
+        result = main.get_post_body(datetime.datetime(2022, 10, 28, 1, 2, 3, 4))
+
+        assert result == expected
+
+    @patch('main.datastore.Client', MagicMock(return_value=MagicMock(get=MagicMock(return_value={'access_token': '343253645'}))))
+    def test_header_is_returned_with_token(self,):
+        expected = {
+            'Authorization': 'Bearer 343253645'
+        }
+
+        result = main.get_header()
+
+        assert result == expected
+
+    @patch('main.datastore.Client', MagicMock(return_value=None))
+    def test_empty_headers_returned_when_datastore_client_not_initialised(self):
+        expected = {}
+
+        result = main.get_header()
+
+        assert result == expected
+
+    @patch('main.datastore.Client', MagicMock(return_value=MagicMock(get=MagicMock(return_value={'token': '343253645'}))))
+    def test_empty_headers_returned_when_datastore_object_does_not_contain_access_token(self):
+        expected = {}
+
+        result = main.get_header()
+
+        assert result == expected
+
+    @patch('main.datastore.Client', MagicMock(return_value=MagicMock(get=MagicMock(return_value=None))))
+    def test_empty_headers_returned_when_datastore_object_is_none(self):
+        expected = {}
+
+        result = main.get_header()
+
+        assert result == expected
+
+    @patch('main.get_header', MagicMock(return_value={}))
+    @patch('main.get_post_body', MagicMock(return_value={}))
+    @patch('main.post', MagicMock(return_value=MagicMock(status_code=200, json=MagicMock(return_value={'bucket': [{'dataset': [{'point': [{'value': [{'intVal': 25}]}, {'value': [{'intVal': 30}]}]}]}]}))))
+    def test_execute_returns_steps_value_from_fit_api(self):
+
+        expected = { 'steps': 55 }
+
+        result = main.execute(None)
+
+        assert result == expected
+
+    @patch('main.get_header', MagicMock(return_value={}))
+    @patch('main.get_post_body', MagicMock(return_value={}))
+    @patch('main.post', MagicMock(return_value=MagicMock(status_code=401)))
+    def test_execute_returns_zero_steps_when_no_response_from_api(self):
+        expected = {'steps': 0}
+
+        result = main.execute(None)
+
+        assert result == expected
+
+
+
